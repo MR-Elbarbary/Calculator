50efcea143844e408931a7a892e220313f7e9af0
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2024-08-05 20:28:58 +0300

    Create add and delete functions

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let operators= ["+", "-", "/", "^", "X", "%"]
+let operator = false
+
+function evaluate(formula) {
+    if (operators.includes(formula[formula.length - 1]) || operators.includes(formula[0])) {
+        return
+    }
+    
+}
+
 function add(num1, num2){
     return num1 + num2
 }
@@ -22,15 +32,48 @@ function mod(num1, num2) {
     return num1 % num2
 }
 
-function AC() {
-    
-}
 
-function Del() {
-    
+function Del(display) {
+    let array = [...display.textContent]
+    if(operators.includes(array[array.length - 1])){
+        operator = false
+    }
+    array.pop();
+    display.textContent = array.join('');
 }
-
 
 document.addEventListener("DOMContentLoaded", () =>{
-    
+    let display = document.querySelector(".display")
+    let normalButtons = document.querySelectorAll(".common")
+    let result = document.querySelector(".result")
+    let del = document.querySelector("#delete")
+    let ac = document.querySelector("#ac")
+    let equals = document.querySelector("#equal")
+
+    equals.addEventListener("click", () => {
+        result.textContent = evaluate(display.textContent)
+    })
+
+    del.addEventListener("click", () =>{ Del(display) })
+
+    ac.addEventListener("click", () =>{
+        display.innerHTML = ""
+        operator = false
+    })
+
+    normalButtons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            if (operators.includes(event.target.innerHTML))
+            {
+                if(operator){
+                    return
+                }
+                else if(display.textContent === ''){
+                    return
+                }
+                operator = true
+            }
+            display.textContent += event.target.textContent
+        })
+    });
 })
